c8dbf5082f3c527f8bffb16b71d8426fb147720a
Thomas Niels Berthelsen <tnb@novicell.dk>
2022-02-17 10:58:35 +0100

    setup main function

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, make_response, g, request, send_file
+
+# Instantiate the Flask app (must be before the endpoint functions)
+app = Flask(__name__)
+
+@app.route('/')
+def Greet():
+    return make_response({'message': 'Hello World!'})
+
+def setupNode():
+    #Setup events
+
+    # Start the Flask app (must be after the endpoint functions)
+    host_local_computer = "localhost" # Listen for connections on the local computer
+    host_local_network = "0.0.0.0" # Listen for connections on the local network
+    app.run(host=host_local_network if False else host_local_computer, port=9000)
