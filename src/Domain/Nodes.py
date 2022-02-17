e18573e7520788f6b4ac51abbb4cd87ac71681cc
Harfeld <andreas.harfeld@gmail.com>
2022-02-17 15:51:40 +0100

    winner winner berthels dinner

--- src/Domain/Nodes.py
+++ src/Domain/Nodes.py
@@ -1,3 +1,4 @@
+import enum
 import threading, time, random, os, sys
 
 import numpy as np
@@ -10,8 +11,19 @@ class Nodes(object):
     _nodesList = []
 
     _haltedBy = 0
-    _states = {"down", "reorganizing", "normal", "election"}
-    _currentState = "down"
+
+    class states(enum.Enum):
+        down = "down"
+        reorganizing = "reorganizing"
+        normal = "normal"
+        election = "election"
+
+    class tasks(enum.Enum):
+        working = "working"
+        stopped = "stopped"
+
+    _currentState = states.normal
+    _currentTask = tasks.working
 
     def __new__(class_, *args, **kwargs):
         if not isinstance(class_._instance, class_):
