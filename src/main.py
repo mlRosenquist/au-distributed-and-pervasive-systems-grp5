b40f802d8981e5b8c01bfbb01766f309c8b9c307
Morten Lyng Rosenquist <lyngrosenquistmorten@gmail.com>
2022-02-24 13:36:53 +0100

    response to string

--- src/main.py
+++ src/main.py
@@ -17,49 +17,49 @@ def greet():
 @app.route('/areYouThere',  methods=['GET'])
 def areYouThereCommand():
     if nodes.isState(nodes.states.down):
-        return make_response(500)
+        return make_response("500")
     else:
         print('I am not down, sending 200')
-        return make_response(200)
+        return make_response("200")
 
 @app.route('/areYouNormal', methods=['GET'])
 def areYouNormal():
     if nodes.isState(nodes.states.normal):
-        return make_response(200)
+        return make_response("200")
     else:
-        return make_response(500)
+        return make_response("500")
 
 @app.route('/halt', methods=['POST'])
 def haltCommand():
     if nodes.isState(nodes.states.down):
-        return make_response(500)
+        return make_response("500")
     else:
         nodes.setState(nodes.states.election)
         jsonContent = request.json()
         nodes.setHaltedBy(jsonContent['sender_j'])
-        return make_response(200)
+        return make_response("200")
 
 @app.route('/newCoordinator', methods=['POST'])
 def updateCoordinator():
     if nodes.isState(nodes.states.down):
-        return make_response(500)
+        return make_response("500")
     else:
         senderId = request.json()['sender_j']
         if nodes.getHaltedBy() == senderId and nodes.isState(nodes.states.election):
             nodes.setCoordinator(senderId)
             nodes.setState(nodes.states.reorganizing)
-        return make_response(200)
+        return make_response("200")
 
 @app.route('/ready', methods=['POST'])
 def ready():
     senderId = request.json()['sender_j']
     taskDescription = request.json()['work_x']
     if nodes.isState(nodes.states.down):
-        return make_response(500)
+        return make_response("500")
     elif nodes.isState(nodes.states.reorganizing) and nodes.getCoordinator() == senderId:
         nodes.setState(nodes.states.normal)
         nodes.setTask(taskDescription)
-    return make_response(200)
+    return make_response("200")
 
 def setupNode():
     #Setup scheduled jobs
