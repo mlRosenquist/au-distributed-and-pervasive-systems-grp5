c641b376ad0fc24b2f7bebd26d621384ce9f8cf7
DESKTOP-55PV0K9\berth <berthel0802@gmail.com>
2022-04-24 20:14:16 +0200

    dasd

--- project2/src/PillSupervisor/Phidget.SensorHandling/Program.cs
+++ project2/src/PillSupervisor/Phidget.SensorHandling/Program.cs
@@ -1,33 +1,74 @@
 ﻿using System;
 using System.Timers;
 using Phidget22;
+using MQTTnet;
+using MQTTnet.Client;
+using Newtonsoft.Json;
 
 namespace Phidget.SensorHandling
 {
-	class Program
+	public class Program
 	{
-		private static void VoltageInput_VoltageChangeLongRange(object sender, Phidget22.Events.VoltageInputVoltageChangeEventArgs e)
+        private static MqttClient? Client { get; set; }
+		
+        private async static void VoltageInput_VoltageChangeLongRange(object sender, Phidget22.Events.VoltageInputVoltageChangeEventArgs e)
 		{
 			Phidget22.VoltageInput evChannel = (Phidget22.VoltageInput)sender;
 			// Long range goes fast from 5 to 0
 			var active = e.Voltage < 1;
-			if (active)
-				Console.WriteLine($"Long range is active!");
+			if (!active)
+				return;
+
+			Console.WriteLine($"Long range is active!");
+
+			var payload = new ProximitySensorEvent()
+			{
+				DeviceId = "101",
+				DeviceDescription = "Long range device",
+				Payload = "Fired!"
+			};
+
+			var payloadJson = JsonConvert.SerializeObject(payload);
+
+			await Client.PublishStringAsync("LongRangeSensor", payload: payloadJson);
 
 
 		}
 
-		private static void VoltageInput_VoltageChangeCloseRange(object sender, Phidget22.Events.VoltageInputVoltageChangeEventArgs e)
+		private async static void VoltageInput_VoltageChangeCloseRange(object sender, Phidget22.Events.VoltageInputVoltageChangeEventArgs e)
 		{
 			Phidget22.VoltageInput evChannel = (Phidget22.VoltageInput)sender;
 			// Close range goes unstable from 5 to 0.5, so bigger step for stability
 			var active = e.Voltage < 2;
-			if (active)
-				Console.WriteLine($"Close is active!");
+			if (!active)
+				return;
+
+			Console.WriteLine($"Close is active!");
+
+			var payload = new ProximitySensorEvent()
+			{
+				DeviceId = "102",
+				DeviceDescription = "Short range device",
+				Payload = "Fired!"
+			};
+
+			var payloadJson = JsonConvert.SerializeObject(payload);
+
+			await Client.PublishStringAsync("ShortRangeSensor", payload: payloadJson);
 		}
 
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
+			// Init MQQT Client
+			var mqttFactory = new MqttFactory();
+			Client = mqttFactory.CreateMqttClient();
+
+			var mqttClientOptions = new MqttClientOptionsBuilder()
+				.WithTcpServer("86.52.53.126")
+				.Build();
+
+			await Client.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
 			VoltageInput voltageInputLongRange = new VoltageInput();
 			VoltageInput voltageInputCloseRange = new VoltageInput();
 
@@ -51,4 +92,11 @@ namespace Phidget.SensorHandling
 			voltageInputCloseRange.Close();
 		}
 	}
+
+    internal class ProximitySensorEvent
+	{
+        public string? DeviceId { get; set; }
+        public string? DeviceDescription { get; set; }
+        public string? Payload { get; set; }
+    }
 }
